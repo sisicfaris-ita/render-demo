815bad638c81bc57d2a28fc837a28bc577b16373
Administrator <administrator@Administrators-iMac-2.local>
2024-07-02 18:15:00 +0200

    feat: added env route

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const {config} = require("dotenv");
+
+config();
 
 const app = express();
 
 app.get("/health", (req, res) => {
     res.send("Server working");
+});
+
+app.get("/env", (req, res) => {
+    res.send("Test value: " + process.env.TEST_VALUE);
 });
 
 app.listen(8080);
